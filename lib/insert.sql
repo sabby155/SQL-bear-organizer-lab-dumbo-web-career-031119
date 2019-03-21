1ad3feac2343513ca11bbe8daa24fa1627d0e424
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-21 01:10:48 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,8 +3,8 @@ VALUES
 ("Mr. Chocolate", 12, "female", "brown", "chill", 1),    
 ("Mr. Chocolate", 12, "female", "brown", "chill", 1),
 ("Mr. Chocolate", 12, "male", "brown", "chill", 1),
-("Rowdy", 15, "male", "brown", "rowdy", false),
-("Tabitha", 20, "female", "brown", "elegant", false),
-("Sergeant Brown", 32, "male", "brown", "tough", false),
-("Grinch", 82, "female", "brown", "green", true),
-("Wendy", 2, "female", "brown", "cute", true)
+("Rowdy", 15, "male", "brown", "rowdy", 0),
+("Tabitha", 20, "female", "brown", "elegant", 0),
+("Sergeant Brown", 32, "male", "brown", "tough", 0),
+("Grinch", 82, "female", "brown", "green", 1),
+("Wendy", 2, "female", "brown", "cute", 1)
