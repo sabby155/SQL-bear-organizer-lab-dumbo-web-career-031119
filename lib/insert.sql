1c62fb6c0997cff6d01121fe3da1bdcd56832fc2
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-21 01:00:57 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,5 +4,5 @@ VALUES ("Mr. Chocolate", 12, "female", "brown", "chill", true),     ("Mr. Chocol
 ("Rowdy", 12, "female", "brown", "chill", true),
 ("Tabitha", 12, "female", "brown", "chill", true),
 ("Sergeant Brown", 12, "female", "brown", "chill", true),
-("Melissa", 12, "female", "brown", "chill", true),
+("Grinch", 12, "female", "brown", "chill", true),
 ("Mr. Chocolate", 12, "female", "brown", "chill", true),
