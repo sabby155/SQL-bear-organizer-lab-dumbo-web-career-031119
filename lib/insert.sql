8647760e00bbe68b735f4be704d6d56b8dca8767
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-21 01:12:40 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,6 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES 
 ("Mr. Chocolate", 12, "female", "brown", "chill", 1),    
-("Mr. Chocolate", 12, "female", "brown", "chill", 1),
 ("Mr. Chocolate", 12, "male", "brown", "chill", 1),
 ("Rowdy", 15, "male", "brown", "rowdy", 0),
 ("Tabitha", 20, "female", "brown", "elegant", 0),
