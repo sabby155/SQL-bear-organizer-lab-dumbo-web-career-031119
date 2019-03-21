e418dccae720d733d3476406d27f88fd5271161c
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-21 00:59:15 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,23 +1,3 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Mr. Chocolate", 12, "female", "brown", "chill", true)
 
-INSERT INTO bears (name)
-VALUES ("Mr. Chocolate")
-
-INSERT INTO bears (name)
-VALUES ("Rowdy")
-
-INSERT INTO bears (name)
-VALUES ("Tabitha")
-
-INSERT INTO bears (name)
-VALUES ("Sergeant Brown")
-
-INSERT INTO bears (name)
-VALUES ("Melissa")
-
-INSERT INTO bears (name)
-VALUES ("Grinch")
-
-INSERT INTO bears (name)
-VALUES ("Wendy")
